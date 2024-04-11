54bb311cd631695dc1f55e1b6f3d0b7e421fffdd
hans torres <hans.torres@98labs.com>
2024-04-12 00:28:07 +0800

    added  data migrations : user and credential scripts

--- go-exer-2-3/migrations/0001_create_tables.up.sql
+++ go-exer-2-3/migrations/0001_create_tables.up.sql
@@ -1,13 +1,20 @@
--- public.credentials definition
-
--- Drop table
-
--- DROP TABLE public.credentials;
-
-CREATE TABLE public.credentials (
-	id serial4 PRIMARY KEY NOT NULL,
-	"user" varchar(20) NULL,
+create table if not exists credentials (
+	id bigserial primary key,
 	url varchar(20) NULL,
 	username varchar(20) NULL,
-	"password" varchar(20) NULL
+	"password" varchar(20) NULL,
+    created_at timestamp(0) with time zone not null default now()
+);
+
+create table if not exists users (
+    id bigserial primary key,
+    name text not null,
+    username text not null,
+    password text not null
+);
+
+create table if not exists user_credential (
+    user_id bigint not null references users (id) on delete cascade,
+    credential_id bigint not null references credentials (id) on delete cascade,
+    constraint user_credential_key primary key (user_id, credential_id)
 );
